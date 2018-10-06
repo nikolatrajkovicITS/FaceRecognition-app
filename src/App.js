f6d3fb2aa4cb6df4cfe3435ff533ab3f8e38bf85
Nikola Trajkovic <nikolatrajkovic.contact@gmail.com>
2018-10-06 12:34:54 +0200

    Finished with building components and introduced particles js lib

--- src/App.js
+++ src/App.js
@@ -3,15 +3,33 @@ import './App.css';
 import Navigation from './components/navigation/Navigation';
 import Logo from './components/logo/Logo';
 import ImageLinkForm from './components/imageLinkForm/ImageLinkForm';
+import Rank from './components/Rank/Rank';
+import Particles from 'react-particles-js';
+
+const particlesOptions = {
+  particles: {
+    number: {
+      value: 40,
+      density: {
+        enable: true,
+        value_area: 900
+      }
+    }
+  }
+}
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+        <Particles className='particles'
+                   params={particlesOptions}
+          />
         <Navigation />
         <Logo />
+        <Rank />
         <ImageLinkForm />
-        {/* <FaceRecognition /> */}
+        {/* <aceRecognition /> */}
       </div>
     );
   }
