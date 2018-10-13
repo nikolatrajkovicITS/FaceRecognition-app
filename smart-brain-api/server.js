6b50530f08c2abe8e2294388b8e4eb0d0a656d4e
Nikola Trajkovic <nikolatrajkovic.contact@gmail.com>
2018-10-13 11:00:34 +0200

    created register, profile and image routes

--- smart-brain-api/server.js
+++ smart-brain-api/server.js
@@ -1,52 +1,86 @@
-const express = require('express');
-const bodyParser = require('body-parser');
+const bodyParser = require("body-parser");
 
 const app = express();
 
 app.use(bodyParser.json());
 const database = {
-    users: [
-        {
-            id: '1',
-            name: 'John',
-            email: 'john@test.com',
-            password: 'cookies',
-            entries: 0,
-            joined: new Date(),
-         },
-         {
-            id: '12',
-            name: 'Doe',
-            email: 'doe@test.com',
-            password: 'cookies',
-            entries: 0,
-            joined: new Date(),
-         }
-    ]
-}
-
-app.get('/', (req, res) => {
-    res.send('This is working');
-})
-
-app.post('/signin', (req, res) => {
-    if (req.body.email === database.user[0].email &&
-        req.body.password === database.users[0].password) {
-      res.json('success');
-    } else {
-        res.status(400).json('error logging in');
+  users: [
+    {
+      id: "1",
+      name: "John",
+      email: "john@test.com",
+      password: "cookies",
+      entries: 0,
+      joined: new Date()
+    },
+    {
+      id: "12",
+      name: "Doe",
+      email: "doe@test.com",
+      password: "cookies",
+      entries: 0,
+      joined: new Date()
     }
-})
+  ]
+};
 
-app.listen(3000, () => {
-    console.log('App is running on port 3000');
+app.get("/", (req, res) => {
+  res.send(database.users);
 });
 
-/* 
-/ --> res = this is working
-/signin --> POST = success/fail
-/register --> POST = user
-/profile/:userId --> GET = user 
-/image --> PUT = userUpdated
+app.post("/signin", (req, res) => {
+  if (
+    req.body.email === database.user[0].email &&
+    req.body.password === database.users[0].password
+  ) {
+    res.json("success");
+  } else {
+    res.status(400).json("error logging in");
+  }
+});
 
-*/
+app.post("/register", (req, res) => {
+  const { email, name, password } = req.body;
+  database.users.push({
+    id: "3",
+    name: name,
+    email: email,
+    password: password,
+    entries: 0,
+    joined: new Date()
+  });
+  res.json(database.users[database.users.length - 1]);
+});
+
+app.get("/profile/:id", (req, res) => {
+  const { id } = req.params;
+  let found = false;
+  database.users.forEach(user => {
+    if (user.id === id) {
+      found = true;
+      return res.json(user);
+    }
+  });
+  if (!found) {
+    res.status(400).json("not found");
+  }
+});
+
+app.put("/image", (req, res) => {
+  const { id } = req.body;
+  let found = false;
+  database.users.forEach(user => {
+    if (user.id === id) {
+      found = true;
+      user.entries++;
+      return res.json(user.entries);
+    }
+  });
+  if (!found) {
+    res.status(400).json("not found");
+  }
+});
+
+app.listen(3000, () => {
+  console.log("App is running on port 3000");
+});
